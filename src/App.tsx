a71760c443b919e29308e5944acbaad367646190
unknown <734456769@qq.com>
2024-06-24 00:48:54 +0800

    main master 22

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
  * @Author: kaka
  * @Date: 2024-06-23 23:52:29
  * @LastEditors: kaka
- * @LastEditTime: 2024-06-24 00:36:24
+ * @LastEditTime: 2024-06-24 00:48:46
  * @FilePath: \vite-react\src\App.tsx
  * @Description:
  * wx:zjm19960705
@@ -22,7 +22,7 @@ function App() {
   console.log('commit 2.0')
   console.log('commit 3.0')
   console.log('commit 4.0')
-  console.log('react')
+  console.log('react master 222')
   return (
     <>
       <div>
